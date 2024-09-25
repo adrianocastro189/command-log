0b550cbb06be2a862d572c7893c100a8da6ecab2
Adriano Castro <adrianostanley@gmail.com>
2024-09-25 09:08:15 -0300

    Add the history.limit setting

--- CommandLog.lua
+++ CommandLog.lua
@@ -1,13 +1,31 @@
 CommandLog = StormwindLibrary_v1_14_0.new({
     colors = {
-      primary = '000000'
+        primary = '000000'
     },
     command = 'commandlog',
     data = 'CommandLogData',
-    name = 'CommandLog'
-  })
+    name = 'CommandLog',
+    settings = {
+        groups = {
+            {
+                id = 'history',
+                settings = {
+                    {
+                        id = 'limit',
+                        label = 'Limit',
+                        description = 'Sets the maximum number of command executions to keep in the history',
+                        type = 'number',
+                        default = 50,
+                        scope = 'player',
+                        accessibleByCommand = true,
+                    },
+                },
+            },
+        },
+    },
+})
 
 local events = CommandLog.events
 
-events:listen(events.EVENT_NAME_PLAYER_LOGIN, function ()
+events:listen(events.EVENT_NAME_PLAYER_LOGIN, function()
 end)
